d007074ec2fd42144d42837ffc1291aae2712d62
Rakesh Vanam <rakeshvanam6868@gmail.com>
2025-06-25 11:11:06 +0530

    error of apos removed

--- app/page.js
+++ app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
             />
             <h2 className="text-[#d4d4d4] font-semibold text-3xl">ClickNGraB Tech</h2>
             <p className="text-[#d4d4d4] text-center text-sm mt-2 max-w-2xl">
-              ClickNGraB Tech is a platform that curates the best tech deals and products from around the web, providing you with the latest and greatest in technology at your fingertips. Whether you're looking for gadgets, software, or tech accessories, ClickNGraB Tech has got you covered.
+              ClickNGraB Tech is a platform that curates the best tech deals and products from around the web, providing you with the latest and greatest in technology at your fingertips. Whether you&apos;re looking for gadgets, software, or tech accessories, ClickNGraB Tech has got you covered.
             </p>
         </div>
          <div className="flex flex-wrap gap-2 justify-center items-center mt-3 p-2">
