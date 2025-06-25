35d10cf90f77103e92a15075dc10007ab349367d
Rakesh Vanam <rakeshvanam6868@gmail.com>
2025-06-25 11:38:55 +0530

    Done the links part

--- app/page.js
+++ app/page.js
@@ -11,27 +11,41 @@ export default function Home() {
         <div className="flex flex-col justify-center items-center p-4 gap-4">
             <Image
               src="/logo.jpg"
-              alt="ClickNGraB Tech Logo"
-              width={100}
-              height={100}
+              alt="ClickNGrab Tech Logo"
+              width={80}
+              height={80}
               className="rounded-full"
             />
-            <h2 className="text-[#d4d4d4] font-semibold text-3xl">ClickNGraB Tech</h2>
-            <p className="text-[#d4d4d4] text-center text-sm mt-2 max-w-2xl">
-              ClickNGraB Tech is a platform that curates the best tech deals and products from around the web, providing you with the latest and greatest in technology at your fingertips. Whether you&apos;re looking for gadgets, software, or tech accessories, ClickNGraB Tech has got you covered.
+            <h2 className="text-[#d4d4d4] font-semibold text-3xl">ClickNGrab Tech</h2>
+            <p className="text-[#d4d4d4] text-center text-base mt-2 max-w-2xl">
+              ClickNGrab Tech is a platform that curates the best tech deals and products from around the web, providing you with the latest and greatest in technology at your fingertips. Whether you&apos;re looking for gadgets, software, or tech accessories, ClickNGrab Tech has got you covered.
             </p>
         </div>
          <div className="flex flex-wrap gap-2 justify-center items-center mt-3 p-2">
-            <Button className="rounded-full">X Twitter</Button>
-            <Button className="rounded-full">X Twitter</Button>
-            <Button className="rounded-full">X Twitter</Button>
-            <Button className="rounded-full">X Twitter</Button>
-            <Button className="rounded-full">X Twitter</Button>
-            <Button className="rounded-full">X Twitter</Button>
-            <Button className="rounded-full">X Twitter</Button>
+            <Button className="rounded-full text-lg flex justify-between items-center"><Image
+              src="/telegram.png"
+              alt="Telegram Logo"
+              width={25}
+              height={25}
+              className="rounded-full"
+            /> Telegram</Button>
+            <Button className="rounded-full text-lg flex justify-between items-center"><Image
+              src="/insta.png"
+              alt="Telegram Logo"
+              width={25}
+              height={25}
+              className="rounded-full"
+            /> Instagram</Button>
+            <Button className="rounded-full text-lg flex justify-between items-center"><Image
+              src="/youtube.png"
+              alt="Telegram Logo"
+              width={25}
+              height={25}
+              className="rounded-full"
+            />YouTube</Button>
          </div>
          <div className="px-3 py-2 my-3 text-white">
-          <Input type="text" placeholder="Search the products..."/>
+          <Input type="text" placeholder="🔍 Search the products..."/>
          </div>
          <div className="w-full h-auto grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 p-2">
             <InstaCard/>
